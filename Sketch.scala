a4eda76cff35bd2c8be5f1c0e4f3877c443cadf6
Tim Wood <fishuyo@gmail.com>
2014-10-25 20:28:08 -0700

    add sketch space

---
+++ Sketch.scala
@@ -0,0 +1,18 @@
+
+package com.fishuyo.seer
+
+import dynamic._
+
+/**
+  * This example sets up a SeerScriptLoader which compiles
+  * and runs the scala script on modification.
+  * 
+  * See "./scripts/live.scala"
+  * 
+  */
+object Sketch extends SeerApp {
+  val live = new SeerScriptLoader("scripts/sketch.scala")
+
+  override def draw(){}
+  override def animate(dt:Float){}
+}
