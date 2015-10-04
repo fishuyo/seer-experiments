20a9d158e991b5337752137c60b51ac4a19fbaa7
Tim Wood <fishuyo@gmail.com>
2015-10-03 17:01:19 -0700

    final installation mode Master

--- quantumlove/Master.scala
+++ quantumlove/Master.scala
@@ -25,16 +25,22 @@ object Master extends SeerApp {
 
   val music1 = Gdx.audio.newMusic(Gdx.files.absolute("/Users/fishuyo/Desktop/silverthreads-01.mp3"));
   val music2 = Gdx.audio.newMusic(Gdx.files.absolute("/Users/fishuyo/Desktop/silverthreads-02.mp3"));
+  val music3 = Gdx.audio.newMusic(Gdx.files.absolute("/Users/fishuyo/Desktop/binaural.mp3"));
+
+  var installationMode = true
 
   var rawaccel1 = Vec3()
   var accel1 = Vec3()
   var env1 = new EnvFollow(20)
+  var env2 = new EnvFollow(20)
   var beat1 = new Threshold(0.1f)
 
   var rawaccel2 = Vec3()
   var accel2 = Vec3()
   var gyro1 = Vec3()
   var gyro2 = Vec3()
+  var lgyro1 = Vec3()
+  var lgyro2 = Vec3()
 
   var files = HashMap[String,String]()
   files += "bubbles" -> "/Users/fishuyo/Desktop/quantumlove/bubbles.mp4"
@@ -182,37 +188,103 @@ object Master extends SeerApp {
     // node2.renderer.environment.blendFunc(SrcAlpha,OneMinusSrcAlpha)
 
   }
+
+  var t = 0f
+  var manualHole = false
+  var phone1bg = 0f
+  var phone2bg = 0f
   override def animate(dt:Float){
+    t += dt 
     try {
     if(!initd) init()
 
     // println(s"${video1.width} x ${video1.height}")
     if(video1.height > 0 ) node.quad.scale.set(1f*video1.width/video1.height, -1, 1)
 
-    accel1.lerpTo(rawaccel1, 0.1)
-    // println(accel1.mag)
-    env1(rawaccel1.mag)
-    // println(s"env: ${env1.value}")
-    beat1(rawaccel1.mag)
-    if(beat1.value > 0f){ 
-      // println("beat!!!!!!!!!!!!!!!!!!")
 
-    }
+    if(installationMode){
 
-    var user = 0
-    for( i <- 0 until 4){
-      if(skeletons(i).tracking){
-        user += 1
-        skeletons(i).updateJoints()
-        val pos = skeletons(i).joints("torso") * 1000
-        pos.z *= -1
-        val cam = node.renderer.camera
-        val out = KPC.worldToScreen(pos) * Vec3(2,2,0) + Vec3(0.0,-0.1,0)
-        // val out = KPC.worldToScreen(pos) * Vec3(cam.viewportWidth, cam.viewportHeight,0) + Vec3(0.0,-0.1,0)
-        user match {
-          case 1 => node.hole0.set(0.5f*(out.x+1f), 0.5f*(-out.y+1f) )
-          case 2 => node.hole1.set(0.5f*(out.x+1f), 0.5f*(-out.y+1f) )
-          case _ => ()
+      accel1.lerpTo(rawaccel1, 0.1)
+      accel2.lerpTo(rawaccel2, 0.1)
+      env1(rawaccel1.mag)
+      env2(rawaccel2.mag)
+
+      var dgyro1 = (gyro1 - lgyro1) / dt
+      var dgyro2 = (gyro2 - lgyro2) / dt
+      lgyro1.set(gyro1)
+      lgyro2.set(gyro2)
+
+      // if(dgyro1.mag > 0.05f && dgyro1.mag < 10f) println(s"dgyro1: ${dgyro1.mag}")
+      // if(dgyro2.mag > 0f && dgyro2.mag < 10f) println(s"dgyro2: ${dgyro2.mag}")
+      beat1(rawaccel1.mag)
+      if(beat1.value > 0f){ 
+        // println("beat!!!!!!!!!!!!!!!!!!")
+      }
+      // if( t > 2f){
+      //   if(dgyro1.mag > 0.08f && dgyro1.mag < 1f){ 
+      //     loadVideo1(randomVideo(),true)
+      //     phone1bg = 1f-phone1bg
+      //     oscPhones.send("/motionbg1", phone1bg)
+      //   }
+      //   if(dgyro2.mag > 0.01f){
+      //     loadVideo2(randomVideo(),true)
+      //     phone2bg = 1f-phone2bg
+      //     oscPhones.send("/motionbg2", phone2bg)
+      //   }
+      // }
+
+      // println(env2.value*4f - 0.89f*4f + 0.15f)
+      music1.setVolume(env2.value*4f - 0.89f*4f + 0.10f)
+      music3.setVolume(1f - (env2.value*4f - 0.89f*4f + 0.1f))
+
+      var user = 0
+      for( i <- 0 until 4){
+        if(skeletons(i).tracking){
+          user += 1
+          skeletons(i).updateJoints()
+          val pos = skeletons(i).joints("torso") * 1000
+          pos.z *= -1
+          val cam = node.renderer.camera
+          val out = KPC.worldToScreen(pos) * Vec3(2,2,0) + Vec3(0.0,-0.1,0)
+          // val out = KPC.worldToScreen(pos) * Vec3(cam.viewportWidth, cam.viewportHeight,0) + Vec3(0.0,-0.1,0)
+          (i+1) match {
+            case 2 => node.hole0.set(0.5f*(out.x+1f), 0.5f*(-out.y+1f) )
+            case 1 => node.div = (out.x+2f)/4f
+            case 3 => video1.setVolume( (out.x+2f)/4f * 0.5)
+                      // OSC.send("/left/fade",(out.x+2f)/4f)
+            case 4 => video2.setVolume( (out.x+2f)/4f * 0.5)
+                      // OSC.send("/right/fade",(out.x+2f)/4f)
+            case _ => ()
+          }
+        }
+      }
+      if(user < 1) node.div = 0.5
+      if(user < 3 && t > 10f){
+        video1.setVolume(0.1f)
+        video2.setVolume(0.1f)
+        // OSC.send("/left/fade", 0.6f)
+        // OSC.send("/right/fade",0.6f) 
+        t=0f       
+      }
+
+    } else {
+      var user = 0
+      for( i <- 0 until 4){
+        if(skeletons(i).tracking){
+          user += 1
+          skeletons(i).updateJoints()
+          val pos = skeletons(i).joints("torso") * 1000
+          pos.z *= -1
+          val cam = node.renderer.camera
+          val out = KPC.worldToScreen(pos) * Vec3(2,2,0) + Vec3(0.0,-0.1,0)
+          // val out = KPC.worldToScreen(pos) * Vec3(cam.viewportWidth, cam.viewportHeight,0) + Vec3(0.0,-0.1,0)
+          if(!manualHole){
+            user match {
+              case 1 => node.hole0.set(0.5f*(out.x+1f), 0.5f*(-out.y+1f) )
+              // case 2 => node.hole1.set(0.5f*(out.x+1f), 0.5f*(-out.y+1f) )
+              case _ => ()
+            }
+          }
         }
       }
     }
@@ -232,52 +304,103 @@ object Master extends SeerApp {
   val oscPhones = new OSCSend
   oscPhones.connect("localhost", 8083)
   // OSC.connect("192.168.0.4", 8008)
-  OSC.connect("169.231.113.146", 8008)
+  OSC.connect("169.231.127.37", 8008)
   OSC.bindp {
     case Message("/gyro1", roll:Float, pitch:Float, yaw:Float) => gyro1.set(pitch,yaw,roll)
     case Message("/gyro2", roll:Float, pitch:Float, yaw:Float) => gyro2.set(pitch,yaw,roll)
     case Message("/accel1", x:Float, y:Float, z:Float) => rawaccel1.set(x,y,z)
     case Message("/accel2", x:Float, y:Float, z:Float) => rawaccel2.set(x,y,z)
+    case Message("/up1", x:Float) => if(installationMode){
+      println("up1")
+      loadVideo1(randomVideo(),true)
+      // oscPhones.send("/motionbg1", 1f) 
+      // OSC.send("/left/fade",1.0f)
+    }
+    case Message("/down1", x:Float) => if(installationMode){
+      println("down1")
+      // OSC.send("/left/fade",0.5f)
+      // oscPhones.send("/motionbg1", 0f) 
+    }
+    case Message("/up2", x:Float) => if(installationMode){
+      println("up2")
+      loadVideo2(randomVideo(),true)
+      // oscPhones.send("/motionbg2", 1f) 
+      // OSC.send("/right/fade",1.0f)
+    }
+    case Message("/down2", x:Float) => if(installationMode){
+      println("down2")
+      // OSC.send("/right/fade",0.5f)
+      // oscPhones.send("/motionbg2", 0f) 
+    }
     
-    case Message("/1/xy", y:Float, x:Float) => node.hole0.lerpTo(Vec2(x,1f-y), 0.1f)
+    case Message("/1/xy/1", y:Float, x:Float) => node.hole0.lerpTo(Vec2(x,1f-y), 0.1f)
     case Message("/1/fader1", f:Float) => node.fade = f
     case Message("/1/fader2", f:Float) => OSC.send("/left/fade",f)
     case Message("/1/fader3", f:Float) => OSC.send("/right/fade",f)
-    case Message("/1/fader4", f:Float) => node.div = f*1.2f - 0.1f
+    case Message("/1/fader4", f:Float) => node.size0 = lerp(node.size0,f*8.0f,0.1f)
+    case Message("/1/toggle4", f:Float) => node.mode0 = f
+    case Message("/1/toggle3", f:Float) => manualHole = (f == 1f)
+
+    case Message("/1/fader5", f:Float) => node.div = f*1.2f - 0.1f
     case Message("/1/push1", f:Float) => if(f == 1f) loadVideo("grass", true)
     case Message("/1/push2", f:Float) => if(f == 1f) loadVideo("underwater3", true)
     case Message("/1/push3", f:Float) => if(f == 1f){
       val v = randomPair()
       loadVideo1(v._1, true)
       loadVideo2(v._2, true)
+      node.div = 0.5
     }
     case Message("/1/push4", f:Float) => if(f == 1f){
       loadVideo1("grass", true)
       loadVideo2("wave", true)
+      node.div = 0.5
     }
     case Message("/1/push5", f:Float) => if(f == 1f) loadVideo("clouds", true)
-    case Message("/2/fader1", f:Float) => node.size0 = lerp(node.size0,f*8.0f,0.1f)
-    case Message("/2/toggle1", f:Float) => node.mode0 = f
+
     // case Message("/2/fader2", f:Float) => node.size1 = lerp(node.size1,f*6.0f,0.1f)
     // case Message("/2/toggle2", f:Float) => node.mode1 = f
-    case Message("/2/fader2", f:Float) => OSC.send("/left/fade",f)
-    case Message("/2/fader3", f:Float) => OSC.send("/right/fade",f)
+    // case Message("/2/fader2", f:Float) => OSC.send("/left/fade",f)
+    // case Message("/2/fader3", f:Float) => OSC.send("/right/fade",f)
 
 
-    case Message("/2/fader7", f:Float) => oscPhones.send("/brightness1", f)
-    case Message("/2/fader8", f:Float) => oscPhones.send("/brightness2", f)
-    case Message("/2/toggle7", f:Float) => oscPhones.send("/motionbg1", f)
-    case Message("/2/toggle8", f:Float) => oscPhones.send("/motionbg2", f)
+    case Message("/2/fader1", f:Float) => oscPhones.send("/brightness1", f)
+    case Message("/2/fader2", f:Float) => oscPhones.send("/brightness2", f)
+    case Message("/2/fader3", f:Float) => OSC.send("/left/rate", f)
+    case Message("/2/fader4", f:Float) => OSC.send("/right/rate", f)
+    case Message("/2/toggle1", f:Float) => oscPhones.send("/motionbg1", 1f-f)
+    case Message("/2/toggle2", f:Float) => oscPhones.send("/motionbg2", 1f-f)
+    case Message("/1/toggle1", f:Float) => oscPhones.send("/motionbg1", 1f-f)
+    case Message("/1/toggle2", f:Float) => oscPhones.send("/motionbg2", 1f-f)
 
-    case Message("/3/fader7", f:Float) => video1.setVolume(f)
-    case Message("/3/fader8", f:Float) => video2.setVolume(f)
-    case Message("/3/fader1", f:Float) => music1.setVolume(f); music2.setVolume(f)
-    case Message("/3/toggle1", f:Float) => if(f==1f) music1.play() else music1.stop()
-    case Message("/3/toggle2", f:Float) => if(f==1f) music2.play() else music2.stop()
+    case Message("/2/fader7", f:Float) => video1.setVolume(f)
+    case Message("/2/fader8", f:Float) => video2.setVolume(f)
+    case Message("/2/fader5", f:Float) => music1.setVolume(f)
+    case Message("/2/fader6", f:Float) => music2.setVolume(f)
+    case Message("/2/fader9", f:Float) => music3.setVolume(f)
+    case Message("/2/toggle5", f:Float) => if(f==1f) music1.play() else music1.stop()
+    case Message("/2/toggle6", f:Float) => if(f==1f) music2.play() else music2.stop()
+    case Message("/2/toggle9", f:Float) => if(f==1f) music3.play() else music3.stop()
+
+    case Message("/2/toggle10", f:Float) => {
+      installationMode = (f==1f)
+      if(installationMode){
+        music1.setLooping(true)
+        music1.setVolume(0.1f)
+        music1.play()
+        music3.setLooping(true)
+        music3.setVolume(1f)
+        music3.play()
+      } else {
+        music1.stop()
+        music1.setLooping(false)
+        music3.stop()  
+      }
+    }
 
     case msg => println(msg)
   }
 
+  var setdraw = 1f
 
   Trackpad.bind { case t => 
 
@@ -318,6 +441,10 @@ object Master extends SeerApp {
   Keyboard.bind("o", ()=>{ if(music1.isPlaying()) music1.pause() else music1.play() })
   Keyboard.bind("p", ()=>{ if(music2.isPlaying()) music2.pause() else music2.play() })
   Keyboard.bind("m", ()=>{ if(node.mode0 == 0f) node.mode0 = 1f else node.mode0 = 0f })
+  Keyboard.bind("j", ()=>{ 
+    OSC.send("/draw",setdraw)
+    if(setdraw == 1f) setdraw = 0f else setdraw = 1f
+  })
 }
 
 class MaskBlendNode extends RenderNode {
